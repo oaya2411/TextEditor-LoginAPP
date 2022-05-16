8192ad2c6a287addea47fce283af97fdf3ec6c57
oaya2411 <101758186+oaya2411@users.noreply.github.com>
2022-05-16 17:26:57 +0200

    Update 20210081.cpp
    comments to all file

--- TextEditor/20210081.cpp
+++ TextEditor/20210081.cpp
@@ -15,6 +15,7 @@ extern ostringstream strstream;
 extern string str;
 bool cf = false;
 
+
 void clearScreen()
 {
     system("CLS");
@@ -22,62 +23,62 @@ void clearScreen()
     system("CLS");
 }
 
-void function1()
+void function1()//this function to add more texts to the main txt file.
 {
 string newtext;
         cout << "Enter the new string to add: ";
-        cin.ignore();
+        cin.ignore();//to ignore any input into the buffer.
         getline(cin, newtext);
-        if (cf)
+        if (cf)//if the main file has a text on it.
         {
-            str += newtext;
-            save(name);
+            str += newtext;//add the new elements to the main file, So str is the string into the main file and the newtext is the string which on the old one. 
+            save(name);//save all the commits.
             cf = false;
         }
-        else 
+        else //else that the main file is empty.
         {
-            str += '\n';
-            str += newtext;
+            str += '\n';//to start with anewline
+            str += newtext;//add the other content into it.
         }
         save(name);
 }
-void function2()
+void function2()//to show the text into the file.
 {
   cout << "\n";
         clearScreen();
         cout << "The Text is:\n";
         cout << "-----------------------------------------------------------------|\n";
-        cout << str;
+        cout << str;//str is all text into the file as it shown on the function of load screen.
         cout << "\n\npress enter to continue: ";
         cin.ignore();
         char temp = 'x';
         while (temp != '\n')
             temp = cin.get();
 }
-void function3()
+void function3()//delete all things into the file.
 {
-    str = "";
+    str = "";//replace all the content with empty content.
 }
 
-void function4(){
+void function4(){//encrypt the file
     string encrypt;
         encrypt = str;
         cout << "\n";
-        char n;
-        str = "";
-        for (int i = 0; i < encrypt.size(); i++) 
+        char n;//to load the encrypted text into a text but character by character.
+        str = "";//empty the string to full the file with new encrypted text.
+        for (int i = 0; i < encrypt.size(); i++) //loop to check all the content of the file.
         {
-            if ((char)encrypt.at(i) != 10 || (char)encrypt.at(i) != 13) 
+            if ((char)encrypt.at(i) != 10 || (char)encrypt.at(i) != 13) //check to ignore the newline.
             {
-                n = char(encrypt.at(i) + 1);
+                n = char(encrypt.at(i) + 1);//if it is a char we replace the old char with new one.
                 dataFile << n;
-                str +=n;
+                str +=n;//add the encrypted char to str.
             }
             else
                 dataFile << "/n";
         }
 }
-void function5(){
+void function5(){//decrypt all data
     string decrypt;
         decrypt = str;
         cout << "\n";
@@ -86,14 +87,14 @@ void function5(){
         for (int i = 0; i < decrypt.size(); i++) 
         {
             if ((char)decrypt.at(i) != 10 || (char)decrypt.at(i) != 13) {
-                n = char(decrypt.at(i) - 1);
+                n = char(decrypt.at(i) - 1);//here we shifted one to back so if the char is d it will be (d--> c).
                 dataFile << n;
                 str +=n;
             }
             else
                 dataFile << "/n";
         }
-        dataFile.close();
+        dataFile.close();//close file to force flush.
 }
 
 
